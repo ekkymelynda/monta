bedccb49347ebaba65907b0590dbf288b019eb09
apapipip <afifsayudha279@gmail.com>
2016-05-25 01:53:44 +0700

    tugas akhir tes

---
+++ resources/views/status tugas akhir/c.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Monitoring Tugas Akhir</title>
+</head>
+<body>
+  <h1>Status Tugas Akhir</h1>
+<form action="">
+  ID Status Tugas Akhir:<br>
+  <input type="text" name="" placeholder=""><br>
+  Nama Status Tugas Akhir:<br>
+  <input type="text" name="" placeholder=""><br>
+  Create date:<br>
+  <input type="date" name="" placeholder=""><br>
+  Last Update:<br>
+  <input type="date" name="" placeholder=""><br>
+  Expired Date:<br>
+  <input type="date" name="" placeholder=""><br>
+
+  <input type="submit" value="Submit">
+</form>
+  </body>
+</html>
---
+++ resources/views/tugas akhir/c.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Monitoring Tugas Akhir</title>
+</head>
+<body>
+	<h1>Peserta Didik</h1>
+<form action="">
+  ID peserta didik:<br>
+  <input type="text" name="" placeholder=""><br>
+  Nama peserta didik:<br>
+  <input type="text" name="" placeholder=""><br>
+  Jenis kelamin:<br>
+  <input type="text" name="" placeholder=""><br>
+  Tanggal lahir:<br>
+  <input type="date" name="" placeholder=""><br>
+  NIM/NRP:<br>
+  <input type="text" name="" placeholder=""><br>
+  Email:<br>
+  <input type="text" name="" placeholder=""><br>
+  Nomor handphone<br>
+  <input type="text" name="" placeholder=""><br>
+  Create date:<br>
+  <input type="date" name="" placeholder=""><br>
+  Update date:<br>
+  <input type="date" name="" placeholder=""><br>
+  Soft delete:<br>
+  <input type="boolean" name="" placeholder=""><br>
+
+  <input type="submit" value="Submit">
+</form>
+	</body>
+</html>
